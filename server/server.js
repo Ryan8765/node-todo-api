4abcedc760936552c6c30331ea5fe6cff3107109
Ryan Haas <Ryan Haas>
2016-12-18 09:30:49 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const _=require('lodash');
+
 //root of the application
-var express    = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 //get mongoose
@@ -106,6 +108,40 @@ app.delete('/todos/:id',  (req, res) => {
 		
 		res.status(400).send();
 	});
+
+});
+
+
+app.patch('/todos/:id',  (req, res) => {
+
+	var id = req.params.id;
+
+	//this only allows "text" and "completed" to be updatd
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if(!ObjectID.isValid(id)) {
+		console.log("not valid");
+		return res.status(404).send();
+	}
+
+	if(_.isBoolean(body.completed) && body.completed) {
+		//returns javascript timestamp
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (todo) => {
+		if(!todo) {
+			console.log("not finding by id");
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch( (e) => {
+		res.status(400).send();
+	})
 
 });
 
