d0f4016b6df95faa04a44dec4fd2c83cbb347311
Ryan Haas <Ryan Haas>
2016-12-22 19:32:00 -0600

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,31 @@
 var env = process.env.NODE_ENV || 'development';
 
-//This allows us to use both a test and a live database to do our test cases in.  So our test cases won't whipe the live database. 
-if(env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if( env === 'development' || env === 'test' ) {
+	//it will automatically parse this to an object
+	var config = require('./config.json');
+	var envConfig = config[env];
+
+	//takes an object and returns all keys as an array then loops over all keys
+	Object.keys(envConfig).forEach( (key) => {
+		process.env[key] = envConfig[key];
+	});
+
 }
+
+
+
+
+
+
+
+
+//This allows us to use both a test and a live database to do our test cases in.  So our test cases won't whipe the live database. 
+//this was replaced with a json file later in the course....config.json
+// if(env === 'development') {
+// 	process.env.PORT = 3000;
+// 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+// 	process.env.PORT = 3000;
+// 	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -63,7 +63,7 @@ UserSchema.statics.findByToken = function(token) {
 	var decoded; 
 
 	try {
-		decoded = jwt.verify(token, 'abc123');
+		decoded = jwt.verify(token, process.env.JWT_SECRET);
 	} catch(e) {
 		return new Promise( (resolve, reject) => {
 			reject();
@@ -82,7 +82,7 @@ UserSchema.methods.generateAuthToken = function() {
 	var user = this;
 	//get access value and token value
 	var access = 'auth';
-	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+	var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
 	user.tokens.push({
 		access: access,
